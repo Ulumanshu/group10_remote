b99d7a374fe4dadd5ee3510dad5d4b4d35f2bad7
Ulumanshu <karolis_kesminas@yahoo.com>
2022-02-05 11:44:32 +0200

    Add recursion sys.recursionlimit() example

--- recursion.py
+++ recursion.py
@@ -1,14 +1,15 @@
 import sys
 
 
-def recursive_func(number):
+def recursive_func(number, count):
+    count = count
     if number != 0:
-        print(number)
-        number = recursive_func(number - 1)
+        print(number, count)
+        number = recursive_func(number, count + 1)
 
     return number
 
 
 if __name__ == '__main__':
-    sys.setrecursionlimit(11000)
-    recursive_func(9)
+    sys.setrecursionlimit(12000)
+    recursive_func(9, 0)
