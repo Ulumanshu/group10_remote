65552ad3c6e7e01e281539cf80de16e1c2dc5c80
Ulumanshu <karolis_kesminas@yahoo.com>
2022-02-05 11:54:02 +0200

    Add recursion factorial

--- recursion.py
+++ recursion.py
@@ -10,6 +10,24 @@ def recursive_func(number, count):
     return number
 
 
+def factorial(n):
+    """Calculates factorial.
+
+    Args:
+        n: the natural number that is the input for the algorithm.
+    Returns:
+        factorial of number n.
+    """
+    if n == 0:
+        return 1
+    else:
+        res = n * factorial(n-1)
+        print(res)
+        return res
+
+
 if __name__ == '__main__':
     sys.setrecursionlimit(12000)
     recursive_func(9, 0)
+    factorial_5 = factorial(5)
+    print(factorial_5)
