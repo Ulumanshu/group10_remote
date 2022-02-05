c1fe48e8eea2822506f94b83748a6e3695cd9312
Ulumanshu <karolis_kesminas@yahoo.com>
2022-02-05 12:10:00 +0200

    Add recursion factorial + dynamic

--- recursion.py
+++ recursion.py
@@ -1,4 +1,19 @@
 import sys
+from functools import wraps  # Koks tikslas naudoti functools wraps - padeda, kai reikia suzinoti apgaubtos funcijos info
+from time import time
+
+
+def timing(f):
+
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func:%r args:[%r, %r] took: %2.4f sec' % (f.__name__, args, kw, te-ts))
+        return result
+
+    return wrap
 
 
 def recursive_func(number, count):
@@ -41,10 +56,40 @@ def factorial_iterative(n):
     return result
 
 
+@timing
+def factorial_dynamic(n, memory={0:1, 1:1}):
+    """Calculates factorial using dynamic programming.
+
+    Args:
+        n: the natural number that is the input for the algorithm.
+        memory: the results dictionary will be updated with each function call.
+    Returns:
+        factorial of number n.
+    """
+    if n in memory:
+        return memory[n]
+    else:
+        memory[n] = n * factorial_dynamic(n-1)
+        return memory[n]
+
+
 if __name__ == '__main__':
     sys.setrecursionlimit(12000)
     recursive_func(9, 0)
     factorial_5 = factorial_recursive(5)
     # print(factorial_5)
     factorial_6 = factorial_iterative(6)
-    print(factorial_6)
+    # print(factorial_6)
+
+    factorial_11 = factorial_dynamic(110)
+    print(factorial_11)
+    factorial_11 = factorial_dynamic(110)
+    print(factorial_11)
+
+    factorial_12 = factorial_dynamic(120)
+    print(factorial_12)
+
+    factorial_12 = factorial_dynamic(120, memory=dict({0: 1, 1: 1}))
+    print(factorial_12)
+
+
