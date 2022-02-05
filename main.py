c9fa613d4335f5a5b178fabee8157963f24aa151
Ulumanshu <karolis_kesminas@yahoo.com>
2022-02-05 09:50:14 +0200

    Add main.py algorithms

--- main.py
+++ main.py
@@ -5,12 +5,19 @@
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    # Task: Suformuluoti mandagu kreipini.
+    # Inputs: tik kintamasis name, tipas string
+    # Result: mandagus kliento pasveikinimas, vardas turetu visad prasideti is didziosios raides
+    # Method: budas, pasiekti tikslui, siuo atveju python string parsing, formatting. (capitalize(), f string
+    # Expression: kokius programavimo metodus naudoti?
+    print(f'Hi, Mr/Mrs {name.capitalize()}')
+
+# Analysing and testing the algorithm - debug find possible errors
+# Evaluating the algorithm effectiveness - measure speed, and ram usage
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('jolanta')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
